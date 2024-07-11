9acd57e6dbab22c53a136c26870590a9d75e5742
Juan José Jiménez Monsalve <jjsanchez689@gmail.com>
2024-07-11 17:39:40 -0500

    first commit: initial project

---
+++ bin/read_api_fake_store.dart
@@ -0,0 +1,5 @@
+import 'package:read_api_fake_store/read_api_fake_store.dart' as read_api_fake_store;
+
+void main(List<String> arguments) {
+  print('Hello world: ${read_api_fake_store.calculate()}!');
+}
---
+++ test/read_api_fake_store_test.dart
@@ -0,0 +1,8 @@
+import 'package:read_api_fake_store/read_api_fake_store.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
